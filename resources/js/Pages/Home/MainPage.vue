1f7e530a40589be672ac41843eaaaefbbf225366
Jeb <ricojajeb.tayobong@gmail.com>
2025-04-02 22:18:27 +0800

    working on the upload of the  prpoject on the host

--- resources/js/Pages/Home/MainPage.vue
+++ resources/js/Pages/Home/MainPage.vue
@@ -3,7 +3,8 @@
         <main class="bg-red-300 p-3 rounded-xl h-[97vh] m-3  pt-20  ">
                 <section class="bg-yellow-300 max-w-7xl m-auto">
                     <header>
-                        <h1>Build Your Codex, Unlock Your Potential.</h1>
+                        <h1>Build Your Codex, Unlock Your Potential. 1212</h1>
+                        <h2>This is the latest fine shit</h2>
                     </header>                  
                 </section>
         </main>
