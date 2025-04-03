37965a51f162f3af31f0f7e91b8c66e687ca18ee
Jeb <ricojajeb.tayobong@gmail.com>
2025-04-03 14:27:28 +0800

    Install a library primevue

--- resources/js/Pages/Home/MainPage.vue
+++ resources/js/Pages/Home/MainPage.vue
@@ -2,9 +2,9 @@
     <MainLayout>
         <main class="bg-red-300 p-3 rounded-xl h-[97vh] m-3  pt-20  ">
                 <section class="bg-yellow-300 max-w-7xl m-auto">
-                    <header>
-                        <h1>Build Your Codex, Unlock Your Potential. 1212</h1>
-                        <h2>This is the latest fine shit</h2>
+                    <header class="text-center pt-10 pb-4">
+                        <h1 class="text-5xl font-extrabold">Build Your Codex, Unlock Your Potential.</h1>
+                        <p class="mt-4 text-xl font-bold">SunRaku's personal Knowledge base for ideas, notes, and information.</p>
                     </header>                  
                 </section>
         </main>
@@ -15,5 +15,5 @@
 
 <script setup>
     import MainLayout from '@/Layouts/MainLayout.vue';
-
+    import { ref } from 'vue';
 </script>
