8c959c4c799257684c1bde72e305d115f1b77858
Jeb <ricojajeb.tayobong@gmail.com>
2025-05-27 15:46:51 +0800

    Add-- Footer -- add footer in the main page add the social links, facebook,github,email,linked, 	modified:   resources/js/Pages/Footer/Footer.vue

--- resources/js/Pages/Footer/Footer.vue
+++ resources/js/Pages/Footer/Footer.vue
@@ -1,16 +1,40 @@
 <template>
        <footer class="bg-black m-3 text-white rounded-lg h-96">
-        <section >
-             
-            <h1>{{ setting.system_name }}</h1>
+   
+            <section class="flex justify-between h-64 px-24">
+                <header class="flex  pt-24 ">
+                    <h1 class="text-3xl font-bold">{{ setting.system_slogan }}</h1>                   
+                </header>
 
-            <nav></nav>
 
-        </section>
+                <section class="h-32 pt-24 ">
+                    <label for="" class="text-lg font-bold">Social:</label>
+                    <nav class="flex gap-4  mt-4 mb-4 ">  
+                        <a :href="setting.facebook" target="_blank" rel="noopener noreferrer" ><i class="pi pi-facebook" style="font-size: 1.5rem"></i></a>
+                        <a :href="setting.linked" target="_blank" rel="noopener noreferrer" ><i class="pi pi-linkedin" style="font-size: 1.5rem"></i></a>
+                        <a :href="setting.github" target="_blank" rel="noopener noreferrer" ><i class="pi pi-github" style="font-size: 1.5rem"></i></a>
+                   </nav> 
+                          <button><i class="pi pi-envelope" style="font-size: 1.3rem">: <span class="text-lg">{{ setting.email }}</span> </i></button>
+                   
+
+                </section>
+               
+            </section>
+
+
+            <Divider/>
+            <section class="flex justify-between px-24">
+                <h1>© 2025 {{ setting.system_name }}. Built with Laravel & Vue.js. All rights reserved.</h1>
+                <h1>Design and developed by: RJC </h1>
+            </section>
+        
     </footer> 
 </template>
 
 <script setup>
+import { ref } from 'vue';
+ import Divider from 'primevue/divider';
+import { Head, Link, router } from '@inertiajs/vue3';
 
 
     const props = defineProps({
