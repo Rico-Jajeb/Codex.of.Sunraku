a7d6a2e5d7c436a7e7020d927db934ff6e04f5b5
Jeb <ricojajeb.tayobong@gmail.com>
2025-06-24 17:41:47 +0800

    Debugging

--- resources/js/Pages/Admin/TechSkill.vue
+++ resources/js/Pages/Admin/TechSkill.vue
@@ -9,6 +9,7 @@
         <main class="pt-10">
             <section class="max-w-7xl  mx-auto  sm:px-6 lg:px-8 pb-8 bg-white overflow-hidden shadow-xl rounded-lg">
                 <h1>skills</h1>
+                <h1>skills</h1>
                
             </section>
         </main>
